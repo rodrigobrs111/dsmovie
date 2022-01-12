d4750b8d7d84626853a61d76796f1cdeb21010ac
Rodrigo Barreto <rodrigobrs111@gmail.com>
2022-01-12 14:58:46 -0300

    Nabar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,30 @@
+import {ReactComponent as GihubIcon} from 'assets/img/Vector.svg' // importando imagem para aproveitar em um componente
+import './styles.css' 
+
+function Navbar(){
+
+return (
+
+
+    <header> 
+    <nav className="Container"> 
+      <div className="dsmovie-nav-content">
+        <h1>DsMovie</h1>
+        <a href="https://github.com/rodrigobrs111/dsmovie/commits/main">
+            <div className="dsmovie-contact-container">
+                <GihubIcon />
+                <p className="dsmovie-contact-link">/devSuperior</p>
+            </div>
+        </a>
+      </div>
+
+   
+
+    </nav>
+  </header>
+
+);
+    
+}
+
+export default Navbar;
